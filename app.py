b0d8ce40b25a3a66dacd6bc46f9755806cc159db
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-12-03 16:51:59 +0000

    Dec 3, 2023, 1:51 PM

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ import os
 
 app = Flask(__name__)
 
-return render_template('index.html')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_extension(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            # Process the file as needed
+            success_message = "File uploaded successfully!"
+            return render_template('index.html', success=success_message)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
