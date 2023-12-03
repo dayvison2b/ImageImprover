c85ba8ec751fbd2171d9a3ffe90fa89192119a37
Dayvison2b <dayvisoncordeiro2001@gmail.com>
2023-12-03 17:02:26 +0000

    Dec 3, 2023, 2:02 PM

--- app.py
+++ app.py
@@ -15,21 +15,7 @@ def allowed_extension(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            return render_template('index.html', error='No file part')
-    
-        file = request.files['file']
-        
-        if file.filename == '':
-            return render_template('index.html', error='No selected file')
-
-        if file and allowed_extension(file.filename):
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filename)
-            return render_template('index.html', success='File uploaded successfully', filename=filename)
-        
-        else:
-            return render_template('index.html', error='Invalid file type')
+        print(request.files)
     return render_template('index.html')
 
 if __name__ == '__main__':
